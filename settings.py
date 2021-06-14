8e3cb95d126f34186b9c3f36fb39be07145a6878
Mert Doğu <mertdoguonder@gmail.com>
2021-06-14 17:32:25 +0300

    Settings (Customizable Part)
    1.0.5

---
+++ settings.py
@@ -0,0 +1,26 @@
+#------ Customizable ------   --------------------- Info ---------------------
+database = 'db.json'         #Database'in Dosya Konumu // The Location Of Database
+mainbaslik = 'Giris Paneli'   #Ana Pencerenin Başlığı // Main App's Title
+
+mainisim = 'İsim'
+mainsifre = 'Şifre'
+isimplaceholder = 'Lütfen İsmi Giriniz..'
+sifreplaceholder = 'Lütfen Şifreyi Giriniz..'
+
+basariliB = 'Basarili Giris!' #Başarılı Giriş Başlığı // Successful Login Title
+basariliNormal = 'Bilgiler Doğru!' #Succesful Text
+basariliIsim = 'İsim : ' #Showing Name
+basariliSifre = 'Şifre : ' #Showing Password
+
+hataliB = 'Hatali Giris!' #Hatalı Şifre Başlık // Wrong Password Title
+hataliNormal = '                    Bilgiler Yanlış!' #En Üstteki Hata Mesajı // Top Error Message
+hataliErrorMsg = '\n  Bilgileri Değistirerek Yazmayı Dene\n      Eğer Bir Hata Var İse Database\nDosyasını[json] Kontrol Etmeyi Unutma!' #Uzun Hata Mesajı // Long Error Message
+
+girisButon = 'Giris' #Giriş Butonu'nun Yazısı // Login Putton's Text
+
+# print('Hello World!')
+# print('by rexulec with py and <3')
+
+#                If something is clearly wrong, please contact me! ↓
+# t.me/rexulec - github.com/rexulec - discord.gg/codare(rexulec#3200) - rexulec.com
+#                          Thx for using the program!
